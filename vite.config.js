dd4056e76a409fdb4d51c0260b18c9b6e64b07a5
tmengwang <tmengwang@tencent.com>
2023-08-30 15:42:54 +0800

    feature: 配置别名

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve("src")
+    }
+  },
   plugins: [vue()]
 })
