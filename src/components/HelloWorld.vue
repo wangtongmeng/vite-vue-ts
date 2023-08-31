3f7b1c4df01388e1375b68839de056562930ddd2
wangtongmeng <tongmengwang@163.com>
2023-08-31 11:17:19 +0800

    feature: 状态管理

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,8 +3,29 @@ import { ref } from "vue"
 // import logoUrl from "../assets/logo.png"
 import logoUrl from "@/assets/logo.png"
 import styleCss from "./HelloWorld.module.css"
+import { useMainStore } from "@/store"
+import { storeToRefs } from "pinia"
 
 const count = ref(0)
+const mainStore = useMainStore()
+// const storeCount = mainStore.count
+const { count: storeCount } = storeToRefs(mainStore)
+
+const add = () => {
+  mainStore.$patch({
+    firstName: "李",
+    lastName: "四",
+    count: mainStore.count + 1
+  })
+}
+
+const add2 = () => {
+  mainStore.$patch((state) => {
+    state.firstName = "王"
+    state.lastName = "五"
+    state.count = state.count + 2
+  })
+}
 
 defineProps<{ msg: string }>()
 </script>
@@ -22,6 +43,16 @@ defineProps<{ msg: string }>()
   <h2>less</h2>
   <h3>sass</h3>
   <div class="postcss"></div>
+
+  <div>
+    <p>name: {{ mainStore.name }}</p>
+    <p>count: {{ mainStore.count }}</p>
+    <p>count: {{ storeCount }}</p>
+    <button @click="mainStore.count++">mainStore.count++</button>
+    <button @click="add()">add()</button>
+    <button @click="add2()">add2()</button>
+    <button @click="mainStore.addAmount(3)">mainStore.addAmount(3)</button>
+  </div>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import "./global.css"
 import "@/request"
+import { createPinia } from "pinia"
 const app = createApp(App)
+app.use(createPinia())
 app.mount("#app")
